a438da6efbfbe313e6004e045d192f959f9aae01
David Godoy Ayala <godoyayaladavid@gmail.com>
2020-09-29 11:35:24 -0300

    base para que trabajes

--- src/app/components/component/cuadro-inicio-sesion/cuadro-inicio-sesion.component.ts
+++ src/app/components/component/cuadro-inicio-sesion/cuadro-inicio-sesion.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-cuadro-inicio-sesion',
@@ -8,13 +10,33 @@ import { Router } from '@angular/router';
 })
 export class CuadroInicioSesionComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  checkoutForm: FormGroup;
+  mensaje:string="";
+  isDivVisible=false;
+
+  constructor(private router: Router) { 
+    this.checkoutForm = this.createFormGroup();
+  }
 
   ngOnInit(): void {
   }
 
-  // tslint:disable-next-line: typedef
   navegarInicio(){
     this.router.navigate(['/screen-home-ingresado']);
   }
+
+  createFormGroup() {
+    return new FormGroup({
+      usuario: new FormControl('', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$")]),
+      password: new FormControl('',[Validators.required])
+    });
+  }
+
+  onSubmit(){
+    this.mensaje="datos completados";
+    this.isDivVisible=true;
+  }
+
+  get usuario() { return this.checkoutForm.get('usuario'); }
+  get password() { return this.checkoutForm.get('password'); }
 }
--- src/app/components/component/formulario-paciente/formulario-paciente.component.ts
+++ src/app/components/component/formulario-paciente/formulario-paciente.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-formulario-paciente',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormularioPacienteComponent implements OnInit {
 
-  constructor() { }
+  checkoutForm: FormGroup;
+  mensaje:string="";
+  isDivVisible=false;
+
+  constructor() { 
+    this.checkoutForm = this.createFormGroup();
+  }
 
   ngOnInit(): void {
   }
 
+  createFormGroup() {
+    return new FormGroup({
+      nombres: new FormControl('', [Validators.required]),
+      rut: new FormControl('', [Validators.required]),
+      apellidos: new FormControl('', [Validators.required]),
+      fechaNac: new FormControl('', [Validators.required]),
+      //password: new FormControl('',[Validators.required])
+    });
+  }
+
+  onSubmit(){
+    this.mensaje="Paciente Agregado";
+    this.isDivVisible=true;
+  }
+
+  get nombres() { return this.checkoutForm.get('nombres'); }
+  get rut() { return this.checkoutForm.get('rut'); }
+  get apellidos() { return this.checkoutForm.get('apellidos'); }
+  get fechaNac() { return this.checkoutForm.get('fechaNac');}
+  //get password() { return this.checkoutForm.get('password'); }
 }
